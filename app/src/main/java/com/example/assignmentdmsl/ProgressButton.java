4e8d7bd6213c122a6a19f96761165274e18fdc6a
pandula <pandula95@gmail.com>
2020-10-29 22:31:46 +0530

    Add fade in animations and fixed issues in bottom sheet

--- app/src/main/java/com/example/assignmentdmsl/ProgressButton.java
+++ app/src/main/java/com/example/assignmentdmsl/ProgressButton.java
@@ -1,9 +1,10 @@
 package com.example.assignmentdmsl;
-
 import android.content.Context;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -13,16 +14,27 @@ class ProgressButton {
     private ProgressBar progressBar;
     private ConstraintLayout constraintLayout;
     Animation fade_in;
+    private TextView textView;
     ProgressButton(Context ct, View view) {
+        fade_in = AnimationUtils.loadAnimation(ct, R.anim.fade_in);
     cardView = view.findViewById(R.id.cardView);
     progressBar = view.findViewById(R.id.progressBar2);
     constraintLayout = view.findViewById(R.id.constraint_layout);
+    textView = view.findViewById(R.id.textView_button);
     }
     void ButtonActivated () {
+        progressBar.setAnimation(fade_in);
         progressBar.setVisibility(View.VISIBLE);
+        constraintLayout.setBackgroundColor(cardView.getResources().getColor(R.color.material_on_primary_disabled));
+        textView.setAnimation(fade_in);
+        textView.setText("");
+
     }
     void ButtonFinished () {
-        constraintLayout.setBackgroundColor(cardView.getResources().getColor(R.color.design_default_color_error));
+        constraintLayout.setAnimation(fade_in);
+        constraintLayout.setBackgroundColor(cardView.getResources().getColor(R.color.colorPrimary));
         progressBar.setVisibility(View.GONE);
+        textView.setAnimation(fade_in);
+        textView.setText("Done");
     }
 }
